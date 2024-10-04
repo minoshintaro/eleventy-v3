f44f6c192aac8b389a0356560946169f82ef3451
minoshintaro <minos@concentinc.jp>
2024-10-05 02:20:42 +0900

    edit

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,15 +1,16 @@
 import { jsxToString } from 'jsx-async-runtime';
+import prettier from 'prettier';
 
 export default function (eleventyConfig: any) {
+  eleventyConfig.addTemplateFormats('11ty.ts,11ty.tsx');
   eleventyConfig.addExtension(['11ty.jsx', '11ty.ts', '11ty.tsx'], {
     key: '11ty.js',
   });
 
-  eleventyConfig.addTemplateFormats('11ty.ts,11ty.tsx');
-
   eleventyConfig.addTransform('tsx', async (content: JSX.Element) => {
     const result = await jsxToString(content);
-    return `<!doctype html>\n${result}`;
+    const html = `<!doctype html>\n${result}`
+    return prettier.format(html, { parser: 'html' });
 });
 
   return {
