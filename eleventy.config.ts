955de440b220d078ee0e1574c087b445dbb72d75
minoshintaro <minos@concentinc.jp>
2024-10-04 15:26:21 +0900

    edit

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,14 +1,16 @@
-import { renderToString } from "jsx-async-runtime";
+import { jsxToString } from "jsx-async-runtime";
 
 export default function (eleventyConfig: any) {
   eleventyConfig.addExtension(["11ty.jsx", "11ty.ts", "11ty.tsx"], {
     key: "11ty.js",
   });
 
-  eleventyConfig.addTransform("tsx", async (content: any) => {
-    const result = await renderToString(content);
+  eleventyConfig.addTemplateFormats("11ty.ts,11ty.tsx");
+
+  eleventyConfig.addTransform("tsx", async (content: JSX.Element) => {
+    const result = await jsxToString(content);
     return `<!doctype html>\n${result}`;
-  });
+});
 
   return {
     dir: {
