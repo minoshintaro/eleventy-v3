1eca0970286d1e0dcaa45fec66843839524c1aa3
minoshintaro <minos@concentinc.jp>
2024-10-04 19:10:52 +0900

    edit

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,21 +1,22 @@
-import { jsxToString } from "jsx-async-runtime";
+import { jsxToString } from 'jsx-async-runtime';
 
 export default function (eleventyConfig: any) {
-  eleventyConfig.addExtension(["11ty.jsx", "11ty.ts", "11ty.tsx"], {
-    key: "11ty.js",
+  eleventyConfig.addExtension(['11ty.jsx', '11ty.ts', '11ty.tsx'], {
+    key: '11ty.js',
   });
 
-  eleventyConfig.addTemplateFormats("11ty.ts,11ty.tsx");
+  eleventyConfig.addTemplateFormats('11ty.ts,11ty.tsx');
 
-  eleventyConfig.addTransform("tsx", async (content: JSX.Element) => {
+  eleventyConfig.addTransform('tsx', async (content: JSX.Element) => {
     const result = await jsxToString(content);
     return `<!doctype html>\n${result}`;
 });
 
   return {
     dir: {
-      input: "src/pages",
-      output: "dist",
+      input: 'src/pages',
+      layouts: '../layouts',
+      output: 'dist',
     },
   };
 }
--- src/layouts/base.11ty.tsx
+++ src/layouts/base.11ty.tsx
@@ -3,19 +3,15 @@ interface Prop {
   content: string;
 }
 
-const Layout = ({ title, content }: Prop): JSX.Element => {
-  return (
-    <html lang="ja">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title}</title>
-      </head>
-      <body>
-        {content}
-      </body>
-    </html>
-  );
-}
-
-export const render = Layout;
+export const render = ({ title, content }: Prop): JSX.Element => (
+  <html lang="ja">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>{title}</title>
+    </head>
+    <body>
+      {content}
+    </body>
+  </html>
+);
